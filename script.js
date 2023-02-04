638cba6b26cf67b747a53a72d1a907f1129fa394
Gary H <3rdeye.ghj@gmail.com>
2023-02-04 12:52:53 -0800

    Adding constants and still working through event listener logic and then focusing on the rest of the psuedo code

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const newButton = addEventListener("click")
+//Delcare all necessary constants and grab needed IDs for creating functionality in the DOM including a 
 
+const addTaskButton = document.getElementById("add-task");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list")
 
+addTaskButton.addEventListener("click", function() {})
 
-function newToDo(){
+//Build a function "create list" that takes in all variable and generates a new list item when user enters in text box and presses button. Every new item is then add to this list
+
+function createList(){
 
 }
+
+
+//Add radio buttons at the beginning to these new listed items. When this radio button is checked, the list item font gets a strike through and call upon a delete button to remove the item from the list
+
+//Add delete button "(X)". When radio button is pressed, delet button appears and can be engaged with by the user. When pressed, list item disappears
