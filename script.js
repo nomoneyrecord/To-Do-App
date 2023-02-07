20295c740facb78d838776d1ab679b1d8b0a1d51
Gary H <3rdeye.ghj@gmail.com>
2023-02-07 11:22:53 -0800

    Created function that will be called upon when user clicks button
    This will create the list by adding a paragraph element into the ul

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-//Delcare all necessary constants and grab needed IDs for creating functionality in the DOM including a 
+//Delcare all necessary constants and grab needed IDs
 
 const addTaskButton = document.getElementById("add-task");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list")
 
+
 addTaskButton.addEventListener("click", function(e) {
+  e.preventDefault();
   const taskValue = taskInput.value;
   if (!taskValue) return; 
 
-  function printTaskInput() {
-    console.log(taskValue);
-  }
+  createList(taskValue);
+});
 
-  printTaskInput();
-  });
+function creatList(taskValue){
+  const newTask = document.createElement("p");
+  
+}
 
+//Add event listener that calls a click function to addTaskButton 
+//Confirm a return value from the input and terminate once designated true/false
 
+//Create a function that returns the input as a new 'li' in the 'ul' element. 
 
+//Add radio buttons at the beginning to these new listed items. 
 
-
-
-
-
-//Build a function that takes in all variables and generates a new list item when user enters in text box and presses button or enter. Every new item is then add to this list
-
-
-
-
-//Add radio buttons at the beginning to these new listed items. When this radio button is checked, the list item font gets a strike through and call upon a delete button to remove the item from the list
-
-//Add delete button "(X)". When radio button is pressed, delete button appears and can be engaged with by the user. When pressed, list item disappears
+//When radio button is checked, the list item font gets a strike through 
+//Add delete button "(X)" to the right of the list item
+//Wire up delete button to delete that list item
