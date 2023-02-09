5c02e81b36fdfa9a910d4dbde1263178c108cfb3
Gary H <3rdeye.ghj@gmail.com>
2023-02-09 13:16:47 -0800

    Still working throught the radio button that strikes through li when pressed

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//Delcare all necessary constants and grab needed IDs
+
 
 const addTaskButton = document.getElementById("add-task");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list")
 
-//Add event listener that calls a click function to addTaskButton 
-//Confirm a return value from the input and terminate once designated true/false
+
 
 addTaskButton.addEventListener("click", function(e) {
   e.preventDefault();
@@ -15,35 +14,40 @@ addTaskButton.addEventListener("click", function(e) {
   createList(taskValue);
 });
 
-//Create a function that returns the input as a new 'li' in the 'ul' element. 
+
 
 function createList(taskValue){
+  const radioBtn = document.createElement("input");
+  radioBtn.type = "radio";
   const newTask = document.createElement("li");
   const paragraph = document.createElement("p");
   paragraph.textContent = taskValue;
 
-  //Add delete button "(Delete)" to the right of the list item
 
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
 
-  //Wire up delete button to delete that list item
+//Wire up Radio button with strike through
+  radioBtn.addEventListener("click", function(){
+    const text = newTask;
+    const result = text.strike();
+    document.getElementById().innerText = result;
+  });
   
+
   deleteButton.addEventListener("click", function(){
     taskList.removeChild(newTask);
   });   
+  newTask.appendChild(radioBtn);
   newTask.appendChild(paragraph);
   newTask.appendChild(deleteButton);
   taskList.appendChild(newTask);
   taskInput.value = "";
-  
 }
 
 
 
 
 
-//Add radio buttons at the beginning to these new listed items. 
 
-//When radio button is checked, the list item font gets a strike through 
 
