15a7aac9326093a7a1a4a5094935637fa2449463
Gary H <3rdeye.ghj@gmail.com>
2023-02-08 18:26:14 -0800

    Got the delete button to work
    Added logic so that when the delete button is pressed, all nodes attached to it terminate

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const addTaskButton = document.getElementById("add-task");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list")
 
+//Add event listener that calls a click function to addTaskButton 
+//Confirm a return value from the input and terminate once designated true/false
 
 addTaskButton.addEventListener("click", function(e) {
   e.preventDefault();
@@ -13,29 +15,35 @@ addTaskButton.addEventListener("click", function(e) {
   createList(taskValue);
 });
 
+//Create a function that returns the input as a new 'li' in the 'ul' element. 
+
 function createList(taskValue){
   const newTask = document.createElement("li");
   const paragraph = document.createElement("p");
-  const deleteButton = document.createElement("button");
-  deleteButton.addEventListener("click", function(){
-    deleteButton.textContent = "Delete"
-    console.log("Delete button was click!");  })
   paragraph.textContent = taskValue;
+
+  //Add delete button "(Delete)" to the right of the list item
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
+  //Wire up delete button to delete that list item
+  
+  deleteButton.addEventListener("click", function(){
+    taskList.removeChild(newTask);
+  });   
   newTask.appendChild(paragraph);
   newTask.appendChild(deleteButton);
   taskList.appendChild(newTask);
   taskInput.value = "";
   
- 
 }
 
-//Add event listener that calls a click function to addTaskButton 
-//Confirm a return value from the input and terminate once designated true/false
 
-//Create a function that returns the input as a new 'li' in the 'ul' element. 
+
+
 
 //Add radio buttons at the beginning to these new listed items. 
 
 //When radio button is checked, the list item font gets a strike through 
-//Add delete button "(X)" to the right of the list item
-//Wire up delete button to delete that list item
+
